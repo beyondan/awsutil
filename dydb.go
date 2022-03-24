0672086e712d74290da8f8979e2646a816d58142
Daniel Byun <danbyun0110@gmail.com>
2022-03-24 12:36:42 -0400

    dydb putItems

---
+++ dydb.go
@@ -0,0 +1,45 @@
+package dydb
+
+import (
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+type Map map[string]interface{}
+
+func PutItems(tableName string, items []Map) {
+	// Initialize a session that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials
+	// and region from the shared configuration file ~/.aws/config.
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	// Add each item to the dynamodb table
+	for _, item := range items {
+		av, err := dynamodbattribute.MarshalMap(item)
+		if err != nil {
+			log.Fatalf("Got error marshalling map: %s", err)
+		}
+
+		// Create item in table Movies
+		input := &dynamodb.PutItemInput{
+			Item:      av,
+			TableName: aws.String(tableName),
+		}
+
+		_, err = svc.PutItem(input)
+		if err != nil {
+			log.Fatalf("Got error calling PutItem: %s", err)
+		}
+	}
+
+	log.Println("Successfully added items to table %s:\n%+100v\n", tableName, items)
+}
