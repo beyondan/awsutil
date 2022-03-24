13a6946f542352af125fc10088b146f4c8beffdc
Daniel Byun <danbyun0110@gmail.com>
2022-03-24 14:52:03 -0400

    DeleteTable added

--- dydb/dydb.go
+++ dydb/dydb.go
@@ -12,7 +12,39 @@ import (
 
 type Map map[string]interface{}
 
-func PutItems(tableName string, items []Map) {
+func DeleteTable(tableName string) {
+	// Initialize a session that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials
+	// and region from the shared configuration file ~/.aws/config.
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	// Delete table
+	deleteParams := &dynamodb.DeleteTableInput{
+		TableName: aws.String(tableName),
+	}
+	_, err := svc.DeleteTable(deleteParams)
+	if err != nil {
+		log.Fatalf("Got error calling DeleteTable: %s", err)
+	}
+
+	// Wait until table is deleted.
+	describeParams := &dynamodb.DescribeTableInput{
+		TableName: aws.String(tableName),
+	}
+	err = svc.WaitUntilTableNotExists(describeParams)
+	if err != nil {
+		log.Fatalf("Got error calling WaitUntilTableNotExists: %s", err)
+	}
+
+	log.Println(fmt.Sprintf("Successfully deleted all items from table %s.", tableName))
+}
+
+func PutItems[T any](tableName string, items *[]T) {
 	// Initialize a session that the SDK will use to load
 	// credentials from the shared credentials file ~/.aws/credentials
 	// and region from the shared configuration file ~/.aws/config.
@@ -24,7 +56,7 @@ func PutItems(tableName string, items []Map) {
 	svc := dynamodb.New(sess)
 
 	// Add each item to the dynamodb table
-	for _, item := range items {
+	for _, item := range *items {
 		av, err := dynamodbattribute.MarshalMap(item)
 		if err != nil {
 			log.Fatalf("Got error marshalling map: %s", err)
@@ -42,5 +74,5 @@ func PutItems(tableName string, items []Map) {
 		}
 	}
 
-	log.Println(fmt.Sprintf("Successfully added items to table %s:\n%+100v\n", tableName, items))
+	log.Println(fmt.Sprintf("Successfully added items to table %s:\n", tableName))
 }
